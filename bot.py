f6687c686ee74fea2329151148a214fbdf2a622a
mebest068-alt <mebest068@gmail.com>
2026-08-18 06:36:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 BOT_TOKEN = os.getenv("8331988232:AAEP2M_TorpiZy5ucYdUFIuY1rISAAoDAkg")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "1706479196"))
 
-bot = Bot(BOT_TOKEN)
+bot = Bot(8331988232:AAEP2M_TorpiZy5ucYdUFIuY1rISAAoDAkg)
 dp = Dispatcher()
 
 # Открытые обращения:
